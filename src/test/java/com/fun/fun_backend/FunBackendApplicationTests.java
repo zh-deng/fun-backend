7ee63a02146bc32ad83a6e067471c7632b7a549b
zh-deng <heromemo@web.de>
2025-04-17 12:39:52 +0200

    add project

---
+++ src/test/java/com/fun/fun_backend/FunBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fun.fun_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
